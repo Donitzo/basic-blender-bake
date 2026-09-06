4fb5f8ceb77194485869e419bcf3ee0147079152
Donitzo <58847200+Donitzo@users.noreply.github.com>
2026-09-06 17:44:06 +0300

    Added light comment.

--- basic_blender_bake.py
+++ basic_blender_bake.py
@@ -643,7 +643,7 @@ class OBJECT_OT_bake_atlas(bpy.types.Operator):
     )
 
     bake_lights: bpy.props.BoolProperty(
-        name='Bake Lights',
+        name='Bake Lights (disable for ingaming lighting)',
         default=False,
     )
 
